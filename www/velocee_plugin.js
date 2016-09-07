65370d6ab87ec68d6804c37317bc146286f230be
dannybk <dbukshpan@gmail.com>
2016-09-07 11:31:25 +0300

    changed to network image

--- www/velocee_plugin.js
+++ www/velocee_plugin.js
@@ -50,11 +50,11 @@ VeloceePlugin.addFloatingButton = function(source, url, rgbacolor) {
             "box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);"+
             "transition:.3s;"+
             "-webkit-tap-highlight-color: rgba(0,0,0,0);"+
-            "}.vlc_fab_main_btn:focus {"+
+            "}/*.vlc_fab_main_btn:focus {"+
             "transform:scale(1.1);"+
             "transform:rotate(45deg);"+
             "-ms-transform: rotate(45deg);"+
-            "-webkit-transform: rotate(45deg);}";
+            "-webkit-transform: rotate(45deg);}*/";
     
     var style = document.createElement('style');
     style.innerHTML = cssContent;
@@ -80,7 +80,8 @@ VeloceePlugin.addFloatingButton = function(source, url, rgbacolor) {
 
 createVeloceeAudioImage = function() {
     var x = document.createElement("IMG");
-    x.src = velocee_speaker_icon;
+    //x.src = velocee_speaker_icon;
+    x.src="http://app.velocee.com/pages/icon_volume_up.png";
     x.width = 40;
     x.height = 40;
     x.style = "text-align:center;margin:auto";
