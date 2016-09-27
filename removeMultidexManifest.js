f6c6f302590f7f945c7eeedb5faf54ceb9b852d2
dannybk <dbukshpan@gmail.com>
2016-09-27 16:25:29 +0300

    added scripts for multidex

---
+++ removeMultidexManifest.js
@@ -0,0 +1,14 @@
+module.exports = function(ctx) {
+    var fs = ctx.requireCordovaModule('fs'),
+        path = ctx.requireCordovaModule('path'),
+        xml = ctx.requireCordovaModule('cordova-common').xmlHelpers;
+
+    var manifestPath = path.join(ctx.opts.projectRoot, 'platforms/android/AndroidManifest.xml');
+    var doc = xml.parseElementtreeSync(manifestPath);
+    if (doc.getroot().tag !== 'manifest') {
+        throw new Error(manifestPath + ' has incorrect root node name (expected "manifest")');
+    }
+    delete doc.getroot().find('./application').attrib['android:name'];
+    //write the updated manifest file
+    fs.writeFileSync(manifestPath, doc.write({indent: 4}), 'utf-8');
+};
